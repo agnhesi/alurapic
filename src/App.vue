b282995c13a84f9d4c58d632485263aed61316bd
Andressa Agnhesi <aramosagnhesi@gmail.com>
2017-06-24 15:47:36 -0300

    Aula 04

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+
+    <h1 class="titulo">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto in fotos">
+
+        <meu-painel :titulo="foto.titulo">  
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>  
+
       </li>
     </ul>
+
   </div>
 </template>
 
 <script>
+  import Painel from './components/shared/painel/Painel.vue';
+
   export default { 
+    components: {
+      'meu-painel' : Painel
+    },
+
     data() {
       return {
         titulo: 'Alurapic',
         fotos: []
       }
     },
+
     created() {
       this.$http.get('http://localhost:3000/v1/fotos')
       .then(res => res.json())
@@ -26,4 +40,26 @@
 </script>
 
 <style>
+  .titulo {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
 </style>
